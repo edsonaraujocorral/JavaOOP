8e3952b5a0b34f5f61c5969adc039c4d36eb6ca1
edson <edsaraujoc@gmail.com>
2018-12-22 14:24:26 -0200

    Implementacao da Classe Cliente/Conta

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Conta conta = new Conta(1337, 223234);
+
+        Cliente primeiroCliente = new Cliente();
+        primeiroCliente.nome = "Edson";
+        primeiroCliente.cpf = "066.202.8690-41";
+        primeiroCliente.profissao = "Desenvolvedor de Software";
+
+        conta.setTitular(primeiroCliente);
+        System.out.println("Agencia - " + conta.getAgencia() + " Numero - " + conta.getNumero());
+        System.out.println("Titular - " + primeiroCliente.nome + " CPF - " + primeiroCliente.cpf);
+
+        conta.deposita(103.00);
+
+        System.out.println("SALDO R$:" + conta.getSaldo());
+
     }
 }
