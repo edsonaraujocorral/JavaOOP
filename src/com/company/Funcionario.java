910b0750a5c76028445798f24e5916501f61e49f
edson <edsaraujoc@gmail.com>
2018-12-22 15:13:20 -0200

    O Problema da Heranca

--- src/com/company/Funcionario.java
+++ src/com/company/Funcionario.java
@@ -5,13 +5,24 @@ public class Funcionario {
     private String nome;
     private String cpf;
     private double salario;
+    private int tipo = 0;
 
-    public Funcionario() {
+    public int getTipo() {
+        return tipo;
+    }
 
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
     }
 
     public double getBonificacao() {
-        return this.salario * 0.1;
+        if(this.tipo == 0) {
+            return this.salario * 0.1;
+        } else if(this.tipo == 1) {
+            return this.salario;
+        } else {
+            return this.salario + 1000.0;
+        }
     }
 
     public String getNome() {
@@ -37,4 +48,5 @@ public class Funcionario {
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,11 @@ public class Main {
         edson.setNome("Edson");
         edson.setCpf("066.202.869-41");
         edson.setSalario(2500.00);
+        edson.setTipo(2);
 
         System.out.println("Nome - " + edson.getNome());
         System.out.println("CPF - " + edson.getCpf());
         System.out.println("Salario - R$: " + edson.getSalario());
+        System.out.println("Bonificacao - R$: " + edson.getBonificacao());
     }
 }
