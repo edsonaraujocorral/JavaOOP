bfe28cb46bcbce6e5c905c41ef4aefc0223627a3
edson <edsaraujoc@gmail.com>
2018-12-22 14:57:54 -0200

    Teste da Classe Funcionario

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,5 +2,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        Funcionario edson = new Funcionario();
+        edson.setNome("Edson");
+        edson.setCpf("066.202.869-41");
+        edson.setSalario(2500.00);
+
+        System.out.println("Nome - " + edson.getNome());
+        System.out.println("CPF - " + edson.getCpf());
+        System.out.println("Salario - R$: " + edson.getSalario());
     }
 }
