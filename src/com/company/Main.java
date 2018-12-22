57231c332ad39e96d7ecb860bed6c6c0e132b32c
edson <edsaraujoc@gmail.com>
2018-12-22 14:54:03 -0200

    Starting project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,6 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        Conta conta = new Conta(1337, 223234);
-
-        Cliente primeiroCliente = new Cliente();
-        primeiroCliente.nome = "Edson";
-        primeiroCliente.cpf = "066.202.8690-41";
-        primeiroCliente.profissao = "Desenvolvedor de Software";
-
-        conta.setTitular(primeiroCliente);
-        System.out.println("Agencia - " + conta.getAgencia() + " Numero - " + conta.getNumero());
-        System.out.println("Titular - " + primeiroCliente.nome + " CPF - " + primeiroCliente.cpf);
-
-        conta.deposita(103.00);
-
-        System.out.println("SALDO R$:" + conta.getSaldo());
-
     }
 }
